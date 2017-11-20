339d801a93e487e4933bdcf62f70ee4a7b6f4ad2
RAFAEL\Rafael <rafaelreza@ymail.com>
2017-11-19 23:02:01 -0600

    Basic logging class thrown in

---
+++ ShippingStore/src/shippingstore/LoggerClass.java
@@ -0,0 +1,11 @@
+package shippingstore;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+
+
+
+public class LoggerClass {
+
+}
